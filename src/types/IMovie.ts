a5fa2556ddc11841b34ab0c42d98e669f575eaa2
noouh1 <ae2303@fayoum.edu.eg>
2026-02-10 19:14:04 +0200

    Final

--- src/types/IMovie.ts
+++
@@ -1,9 +0,0 @@
-export interface IMovie {
-    title: string;
-    vote_average: number;
-    vote_count: number;
-    release_date: string;
-    backdrop_path?: string;
-    poster_path?: string;
-    overview?: string;
-}
